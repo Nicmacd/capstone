171ec0259aa8ad475a5e87c99c2f9e81cb57890a
19alg2@queensu.ca (19ALG2) <19alg2@queensu.ca>
2024-02-11 13:11:22 -0500

    Added code to divide the segmented data into test and train sets. Also changed plotting so that axis, titles and white space are not included to hopefully improve performance of the model. Additionally tried to clean up the way we were calling functions and saving the different graphs. We are now saving the spectograms, mel and mfcc graphs for both train and test.

--- visualize_mfcc.py
+++ visualize_mfcc.py
@@ -4,9 +4,9 @@ import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_mfcc(audio_file):
+def plot_mfcc(source_folder, audio_file, destination_folder):
     # load audio file
-    signal, sr = librosa.load(audio_file)
+    signal, sr = librosa.load(source_folder + audio_file)
     #num_samples = signal.shape
 
     # extract mfcc's
@@ -18,5 +18,10 @@ def plot_mfcc(audio_file):
     plt.figure(figsize=(25, 10))
     librosa.display.specshow(mfccs, x_axis='time', sr=sr)
     plt.colorbar(format='%+2f')
+
+    # make it so that you are only getting the "image" section do not what to feed extra information into the model
+    plt.axis('off')
+    plt.tight_layout()
+    plt.savefig(destination_folder + audio_file + '_spectogram.png', bbox_inches='tight', pad_inches=0.1)
     plt.show()
 
