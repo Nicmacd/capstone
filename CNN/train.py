d1f4cc90e728ae7155850dabce6a91b670ce6ecb
Carter Conboy <conboy2001@gmail.com>
2024-02-13 14:19:03 -0500

    Finished Training and Testing

--- CNN/train.py
+++ CNN/train.py
@@ -10,35 +10,35 @@ from dataset import Marine_Mammal_Dataset
 from model import CNN
 from torchinfo import summary
 
+# Set Device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(device)
 
-def train(model, train_loader, criterion, optimizer, epoch, num_batch):
+def train(model, train_loader, criterion, optimizer, epochs, num_batch):
     model.train()
     loss_array = np.zeros(epochs)
     for epoch in range(0, epochs):
         print("epoch", epoch)
         loss_sum = 0
-        for batch in range(0, num_batch):
-            for inputs, labels in train_loader:
+        count = 0
+        for inputs, labels in train_loader:
+            inputs =  inputs.to(device)
+            labels =  labels.to(device)
+            with torch.set_grad_enabled(True):
 
-                inputs =  inputs.to(device)
-                labels =  labels.to(device)
-                with torch.set_grad_enabled(True):
+                outputs = model.forward(inputs)
+                loss = criterion(outputs, labels)
 
-                    outputs = model.forward(inputs)
-                    loss = criterion(outputs, labels)
+                # Backward pass: compute gradient of the loss with respect to model parameters
+                optimizer.zero_grad()
+                loss.backward()
 
-                    # Backward pass: compute gradient of the loss with respect to model parameters
-                    optimizer.zero_grad()
-                    loss.backward()
+                optimizer.step()
 
-                    optimizer.step()
-
-                print('Batch Loss:', loss.item())
-                loss_numpy = loss.cpu().detach().numpy()
-                loss_sum = loss_sum + loss_numpy
-            loss_array[epoch] = loss_sum
+            print("Batch [" + str(count) + "/" + str(num_batch) + "]: " + str(loss.item()))
+            count = count + 1
+            loss_numpy = loss.cpu().detach().numpy()
+            loss_sum = loss_sum + loss_numpy
+        loss_array[epoch] = loss_sum
 
     # Plot the batch loss after each iteration
     plt.figure(figsize=(8, 5))
@@ -51,14 +51,15 @@ def train(model, train_loader, criterion, optimizer, epoch, num_batch):
 
 # Example data and targets
 data_train = "../data/mfccData/train/"
+model_path = "./model.pth"
 
 # Define transformations if needed
 transform = transforms.Compose([transforms.ToTensor()])  # Example transformation
 
 # Create a dataset
 dataset_train = Marine_Mammal_Dataset(data_train)
-epochs = 10
-batch_size = 16
+epochs = 5
+batch_size = 32
 num_batch = int(len(dataset_train) / batch_size)
 
 # Create DataLoader for training and testing sets
@@ -68,9 +69,11 @@ train_loader = DataLoader(dataset_train, batch_size=batch_size, shuffle=True)
 model = CNN(dataset_train[0][0].shape)
 model = model.to(device)
 
-criterion = nn.MSELoss()
+criterion = nn.MSELoss() #Should probably use some type of weighted cross entropy here due to classification task with class imbalance
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 train(model, train_loader, criterion, optimizer, epochs, num_batch)
+torch.save(model.state_dict(), model_path)
+
 
 
